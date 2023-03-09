a8ea395e598f59a86208ec56c9f150bd964dd390
LuigiGrimaldi <grimaldi1110@gmail.com>
2023-03-09 19:49:12 +0100

    html+js commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,30 @@
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
+
+//chiedo all'utente quanti km vuole percorrere e console.log
+const howmanyKm = prompt('Quanti km si vogliono percorrere?');
+console.log(howmanyKm);
+
+//chiedo all'utente l'età e console.log
+const userAge = prompt('età?');
+console.log(userAge);
+
+//Costo per km
+let priceKm = 0.21
+let priceKm_under = 0.17
+let priceKm_over = 0.13
+
+if (userAge <= 18) {
+    let tot = priceKm_under * howmanyKm;
+    console.log(tot);
+    document.getElementById('by').innerHTML = 'Il costo del biglietto è: ' + Math.round(Number(tot)) + ' €';
+} else if (userAge >= 65) {
+    let tot = priceKm_over * howmanyKm;
+    console.log(tot);
+    document.getElementById('by').innerHTML = 'Il costo del biglietto è: ' + Math.round(Number(tot)) + ' €';
+} else {
+    let tot = priceKm * howmanyKm;
+    console.log(tot);
+    document.getElementById('by').innerHTML = 'Il costo del biglietto è: ' + tot + ' €';
+}
